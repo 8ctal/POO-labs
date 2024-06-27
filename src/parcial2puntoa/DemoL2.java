5eda4af1ec5fd304e383b3d95c5ce71ad3abf4a4
Camilo Ávila <98609727+8ctal@users.noreply.github.com>
2024-06-26 23:51:20 -0500

    Modifying some variables into english language

--- src/parcial2puntoa/DemoL2.java
+++ src/parcial2puntoa/DemoL2.java
@@ -3,12 +3,12 @@ package parcial2puntoa;
 public class DemoL2 {
 
     public static void main(String[] args) {
-        BaseClientes n = new BaseClientes();
-        n.addCliente(new NoRegistrado("Lucas", "Vallejo", 500000,10));
-        n.addCliente(new NoRegistrado("Andres", "Perez", 500000,18));
-        n.addCliente(new Registrado("Tatiana", "Delgado", 500000,10));
-        n.addCliente(new Registrado("Kanny", "Gelvez", 500000,23));
-        n.imprimirClientes();
+        BaseClientes clientBase = new BaseClientes();
+        clientBase.addCliente(new NoRegistrado("Lucas", "Vallejo", 500000,10));
+        clientBase.addCliente(new NoRegistrado("Andres", "Perez", 500000,18));
+        clientBase.addCliente(new Registrado("Tatiana", "Delgado", 500000,10));
+        clientBase.addCliente(new Registrado("Kanny", "Gelvez", 500000,23));
+        clientBase.imprimirClientes();
     }
     
 }
